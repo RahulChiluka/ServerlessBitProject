1ac1f055b43a6fa75a2759395212fc360d81c628
Rahul Chiluka <rahul13_soe@jnu.ac.in>
2022-06-21 00:31:27 +0530

    Modified twocatz HTTP trigger Azure function

--- twocatz/index.js
+++ twocatz/index.js
@@ -3,17 +3,38 @@ const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const resp = await fetch("https://cataas.com/cat/cute/says/Bitcamp",{
-        method: 'GET'
-    });
+    async function getcatpic() {
+        const resp = await fetch("https://cataas.com/cat/cute/says/Bitcamp",{
+            method: 'GET'
+        });
     
-    const data = await resp.arrayBuffer();
-    // we need to receive it as a buffer since this is an image we are receiving from the API
-    // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
-    var base64data = Buffer.from(data).toString('base64')
+        let data = await resp.arrayBuffer();
+        // we need to receive it as a buffer since this is an image we are receiving from the API
+        // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
+        var base64data = Buffer.from(data).toString('base64')
+        
+        return base64data
+    }
+
+    function getnames() {
+        var names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
+        var random_value = Math.floor(names.length * Math.random())
+        var resultname = names[random_value]
+        return resultname
+    }
+    
+    var firstcat = await getcatpic();
+    var secondcat = await getcatpic();
+    var name1 = getnames();
+    var name2 = getnames();
+
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: { base64data }
+        body: { 
+            cat1: firstcat,
+            cat2: secondcat,
+            names: [name1, name2]
+        }
     };
 }
